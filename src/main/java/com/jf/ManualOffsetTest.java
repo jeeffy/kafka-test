19a68e79fe2def85894a0974fc9c163f92e4a006
jeeffy <jeeffy@126.com>
2016-01-19 10:09:55 +0800

    add manual offset test

--- src/main/java/com/jf/ManualOffsetTest.java
+++ src/main/java/com/jf/ManualOffsetTest.java
@@ -5,26 +5,34 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 /**
- * Created by jeeffy on 1/18/16.
+ * Created by jeeffy on 1/19/16.
  */
-public class ConsumerTest {
+public class ManualOffsetTest {
     public static void main(String[] args) throws Exception {
         Properties props = new Properties();
         InputStream inputStream = ConsumerTest.class.getResourceAsStream("/consumer.properties");
         props.load(inputStream);
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
 
+        int commitInterval = 200;
         consumer.subscribe(Arrays.asList("test", "bar"));
+        List<ConsumerRecord<String, String>> buffer = new ArrayList<ConsumerRecord<String, String>>();
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(100);
-            for (ConsumerRecord<String, String> record : records){
-                System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value());
-                System.out.println();
-
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println(record);
+                buffer.add(record);
+                if (buffer.size() >= commitInterval) {
+                    //insertIntoDb(buffer);
+                    consumer.commitSync();
+                    buffer.clear();
+                }
             }
         }
     }
--- src/main/java/com/jf/ProducerTest.java
+++ src/main/java/com/jf/ProducerTest.java
@@ -16,16 +16,8 @@ public class ProducerTest {
         Properties props = new Properties();
         InputStream inputStream = ProducerTest.class.getResourceAsStream("/producer.properties");
         props.load(inputStream);
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("acks", "all");
-        props.put("retries", 0);
-        props.put("batch.size", 16384);
-        props.put("linger.ms", 1);
-        props.put("buffer.memory", 33554432);
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-
         Producer<String, String> producer = new KafkaProducer(props);
+
         for(int i = 0; i < 100; i++)
             producer.send(new ProducerRecord<String, String>("test", Integer.toString(i), Integer.toString(i)));
 
