d4e2de76ddf3a593505411b7c29fb5c78523734a
jeeffy <jeeffy@126.com>
2016-11-28 09:35:55 +0800

    update pom.xml

--- src/main/java/com/jeeffy/test/KafkaProduceSample.java
+++ src/main/java/com/jeeffy/test/KafkaProduceSample.java
@@ -1,11 +1,10 @@
 package com.jeeffy.test;
 
+import java.util.Properties;
+
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-
-import java.util.Arrays;
-import java.util.Properties;
 
 /**
  * 先启动consumer,再启动producer测试
@@ -19,14 +18,20 @@ public class KafkaProduceSample {
 
 	public static void produce() {
 		Properties props = new Properties();
-		props.put("bootstrap.servers", "99.12.156.134:9092,99.12.156.135:9092");
+		props.put("bootstrap.servers", "kf1:9092,kf2:9092");
 		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
 		Producer<String, String> producer = new KafkaProducer<String, String>(props);
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 10000; i++) {
 			ProducerRecord<String, String> record = new ProducerRecord<String, String>(TOPIC_NAME, "message" + i);
 			producer.send(record);
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		producer.flush();
 		producer.close();
