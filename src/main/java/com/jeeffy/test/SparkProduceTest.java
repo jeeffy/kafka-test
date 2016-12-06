33a8272c16530c818e66ee787f7717ffa9e1a475
jeeffy <jeeffy@126.com>
2016-12-06 15:48:11 +0800

    add spark produce

---
+++ src/main/java/com/jeeffy/test/SparkProduceTest.java
@@ -0,0 +1,39 @@
+package com.jeeffy.test;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+/**
+ * 先启动consumer,再启动producer测试
+ */
+public class SparkProduceTest {
+	public static final String TOPIC_NAME = "test";
+
+	public static void main(String[] args) {
+		produce();
+	}
+
+	public static void produce() {
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "kf1:9092,kf2:9092");
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+		Producer<String, String> producer = new KafkaProducer<String, String>(props);
+		for (int i = 0; i < 10000; i++) {
+			ProducerRecord<String, String> record = new ProducerRecord<String, String>(TOPIC_NAME, "message" + i);
+			producer.send(record);
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		producer.flush();
+		producer.close();
+	}
+}
